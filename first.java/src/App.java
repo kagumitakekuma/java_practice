00048473a654a46e1e126c9edfff6fb15b67806e
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-09 14:38:59 +0900

    Add files via upload

--- first.java/src/App.java
+++ first.java/src/App.java
@@ -4,5 +4,15 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+
+
+
+
+
+
+
+
+
+        
     }
 }
