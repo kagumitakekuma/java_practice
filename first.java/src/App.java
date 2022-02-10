072198f4f5536bd73f3b5ae70fe5f50d03552b0c
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-10 10:22:53 +0900

    Add files via upload

--- first.java/src/App.java
+++ first.java/src/App.java
@@ -5,14 +5,34 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
 
-
-
-
-
-
-
-
-
-        
     }
+}
+
+class Main {
+  public static void main(String[] args) {
+    hello();
+  }
+  
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 引数に「Kate Jones」を渡してください
+    printData("Kate Jones");
+    
+    // 引数に「John Christopher Smith」を渡してください
+    printData("John Christopher Smith");
+    
+  }
+
+  // 引数を受け取るようにしてください
+  public static void printData(String name) {
+    // 「私の名前は◯◯です」と出力されるように書き換えてください
+    System.out.println("私の名前は" +name+"です");
+    
+  }
 }
