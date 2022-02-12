e678c6c49bc9851d4aeee6e0253b9557e63d41bc
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-12 10:43:19 +0900

    Add files via upload

--- first.java/src/App.java
+++ first.java/src/App.java
@@ -32,7 +32,22 @@ class Main {
   // 引数を受け取るようにしてください
   public static void printData(String name) {
     // 「私の名前は◯◯です」と出力されるように書き換えてください
-    System.out.println("私の名前は" +name+"です");
+    System.out.println("私の名前は" + name + "です");
+
+  }
+    public static void main(String[] args) {
+    // それぞれ年齢に関する引数を追加してください
+    printData("Kate Jones",27);
+    printData("John Christopher Smith",65);
+  }
+
+  // 年齢に関する引数を受け取れるようにしてください
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    // 「年齢は◯◯歳です」と出力してください
+    System.out.println("年齢は" +age+ "歳です");
     
   }
+
+
 }
